a074a9d8d301ff714f80224e42d94337d09afe12
Nicolas Garcia Gorchs <nicolasgarciagorchs@gmail.com>
2023-10-24 20:38:27 -0300

    Error en consola

--- src/public/js/createUser.js
+++ src/public/js/createUser.js
@@ -2,7 +2,9 @@ const form = document.getElementById('datosFormulario')
 const response = document.getElementById('response')
 
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', async event => {
+    try {
+    
     event.preventDefault()
 
     const data = new FormData(form)
@@ -10,15 +12,36 @@ form.addEventListener('submit', event => {
     const obj = {}
 
     data.forEach((value, key) => obj[key] = value) // RECIBE (value, key)
+    
+    const headers = {
+        'Content-Type': 'application/json', // SE ESTA ENVIANDO EN EL BODY FORMATO JSON
+    }
 
-    fetch('/create', {
-        headers: { // INFORMACION PARA EL CONTROLADOR(api)
-            'Content-Type': 'application/json', // SE ESTA ENVIANDO EN EL BODY FORMATO JSON
-        },
-        method:'POST', // METODO A UTILIZAR
-        body: JSON.stringify(obj),// EN LA RED SE COMUNICA POR JSON, NO POR JS
+    const method = 'POST'
+
+    const body = JSON.stringify(obj)
+
+    const response = await fetch('/users/create',{
+        headers,
+        method,
+        body,
     })
-    .then(response => response.json())
-    .then(data => (console.log(response) = data.message))
-    .catch(error => console.log(error))
+
+    const formDataa = await response.json()
+
+    console.log(formDataa)
+
+}   catch (error) {
+    console.log(error);
+}
+    // fetch('/users', {
+    //     headers: { // INFORMACION PARA EL CONTROLADOR(api)
+            
+    //     },
+    //     method:'POST', // METODO A UTILIZAR
+    //     body: JSON.stringify(obj),// EN LA RED SE COMUNICA POR JSON, NO POR JS
+    // })
+    // .then(response => response.json())
+    // .then(data => (console.log(response) = data.message))
+    // .catch(error => console.log(error))
 })
